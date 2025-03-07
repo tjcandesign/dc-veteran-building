9324e6ce9393ece836cf8bb020b3f23a2206a4ff
Tj Cichecki <tjcichecki@tjs-macbook-pro.myfiosgateway.com>
2025-03-06 23:28:09 -0500

    Add parallax scrolling effect to hero background on desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     <div>
       {/* Hero Section */}
       <section className="relative bg-dark-brown text-white min-h-[100vh] md:min-h-0 xl:min-h-[90vh] md:py-32 overflow-hidden flex items-center">
-        <div className="absolute inset-0 bg-[url('/images/hero.jpg')] bg-cover bg-center"></div>
+        <div className="absolute inset-0 bg-[url('/images/hero.jpg')] bg-cover bg-center md:bg-fixed"></div>
         <div className="absolute inset-0 bg-black/60 mix-blend-multiply"></div>
         <div className="absolute inset-0 bg-[url('/images/hero-pattern.svg')] opacity-10 mix-blend-overlay"></div>
         <div className="w-full px-4 xl:px-24 mx-auto relative">
